21adb16cc5f084d3b33d604ccc957367e6ccd5ee
hunj <hunjaelee@protonmail.com>
2023-09-08 14:13:00 -0400

    only process portrait photos

--- resizer.py
+++ resizer.py
@@ -1,17 +1,26 @@
+import os
 from PIL import Image, ImageFilter
 import argparse
 
 
+def _ratio(image):
+    return image.height / image.width
+
+
 def square_img(image_path):
+    print(image_path)
     filename = "".join(image_path.split('.')[:-1])
     extension = image_path.split('.')[-1]
 
     original_image = Image.open(image_path)
     exif_data = original_image.info.get('exif')
-    ratio = original_image.height / original_image.width
+    ratio = _ratio(original_image)
+
+    if ratio < 1:
+        return
 
     result_image = original_image.copy()
-    result_image = result_image.rotate(90) if ratio < 1 else result_image
+    result_image = result_image.rotate(90)
 
     zoomed_size = (round(original_image.width * ratio), round(original_image.height * ratio))
     result_image = original_image.resize(size=zoomed_size)
@@ -22,23 +31,20 @@ def square_img(image_path):
 
     result_image.paste(original_image, (round((original_image.height - original_image.width) / 2 ), 0))
 
-    result_image = result_image.rotate(-90) if ratio < 1 else result_image
     result_image.save(fp=f"{filename}_edit.{extension}", quality=100, exif=exif_data)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('images',
-            metavar='I',
-            nargs='+',
-            type=str, help='path to images')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('images', metavar='I', nargs='+', type=str, help='path to images')
     args = parser.parse_args()
 
     for image_path in args.images:
-        if image_path[1] == ":":
-            image_path = "/mnt/{drive}/{path}".format(
-                    drive=image_path[0].lower(),
-                    path="/".join(image_path.split("\\")[1:])
-            )
-        print(image_path)
-        square_img(image_path)
+        if os.path.isdir(image_path):
+            for image_filename in os.listdir(image_path):
+                if image_filename.endswith('.jpg') or image_filename.endswith('.png'):
+                    print(image_filename)
+                    square_img(os.path.join(image_path, image_filename))
+        elif os.isfile(image_path):
+            print(image_path)
+            square_img(image_path)
