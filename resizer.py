8c2779ab43e7600771b36df6f90ada797817e875
hunj <hunjaelee@protonmail.com>
2024-01-29 14:08:06 -0500

    be able to handle instagram 4:5 portrait ratio

--- resizer.py
+++ resizer.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageFilter
 import argparse
 
 
-def square_img(image_path):
+def blur_background(image_path, format="portrait"):
     print(image_path)
     filename = "".join(image_path.split('.')[:-1])
     extension = image_path.split('.')[-1]
@@ -20,12 +20,17 @@ def square_img(image_path):
 
     zoomed_size = (round(original_image.width * ratio), round(original_image.height * ratio))
     result_image = original_image.resize(size=zoomed_size)
+
     length_to_crop = round((result_image.height - original_image.height) / 2)
-
     crop_box = (0, length_to_crop, result_image.width, result_image.height - length_to_crop)
-    result_image = result_image.crop(crop_box).filter(ImageFilter.GaussianBlur(radius=round(original_image.height / (ratio * 100))))
 
+    result_image = result_image.crop(crop_box).filter(ImageFilter.GaussianBlur(radius=round(original_image.height / (ratio * 100))))
     result_image.paste(original_image, (round((original_image.height - original_image.width) / 2), 0))
+
+    if format == "portrait":
+        width_to_crop = result_image.width / 10
+        width_crop_box = (width_to_crop, 0, result_image.width - width_to_crop, result_image.height)
+        result_image = result_image.crop(width_crop_box)
 
     result_image.save(fp=f"{filename}_edit.{extension}", quality=100, exif=exif_data, icc_profile=bytes(icc_profile))
 
@@ -39,8 +44,6 @@ if __name__ == "__main__":
         if os.path.isdir(image_path):
             for image_filename in os.listdir(image_path):
                 if image_filename.endswith('.jpg') or image_filename.endswith('.png'):
-                    print(image_filename)
-                    square_img(os.path.join(image_path, image_filename))
+                    blur_background(os.path.join(image_path, image_filename))
         elif os.path.isfile(image_path):
-            print(image_path)
-            square_img(image_path)
+            blur_background(image_path)
